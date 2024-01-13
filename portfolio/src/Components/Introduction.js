58673bf7301078aeaf13895cf5dca5ceb15cf312
Rylan Cox <rylanrcox@gmail.com>
2024-01-13 14:46:19 -0800

    Finish Navbar

--- portfolio/src/Components/Introduction.js
+++ portfolio/src/Components/Introduction.js
@@ -1,11 +1,11 @@
 const Introduction = () => {
     return (
-        <div id = "Introduction" class = "w-full h-screen bg-violet-900 flex items-center justify-center flex-col">
+        <div id = "Introduction" class = "w-full h-screen bg-violet-900 flex items-center justify-center flex-col text-center">
             <div>
-                <h1 class = "font-mono text-9xl font-extrabold text-white">Welcome!</h1>
+                <h1 class = "font-mono text-7xl font-extrabold text-white">Welcome!</h1>
             </div>
             <div>
-                <h1 class = "font-mono text-6xl font-extrabold text-white">My name is Rylan and I am a Software Developer.</h1>
+                <h1 class = "font-mono text-3xl font-extrabold text-white mt-5">My name is Rylan and I am a Software Developer.</h1>
             </div>
         </div>
     );
--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -1,12 +1,18 @@
 const Navbar = () => {
     return (
-      <nav className="bg-blue-500 text-white p-4">
-        <div className="container mx-auto flex justify-between">
-            <div className="flex space-x-4">
-                <a href="#Introduction" className="hover:bg-blue-700 px-3 py-2 rounded-md">Introduction</a>
-                <a href="#AboutMe" className="hover:bg-blue-700 px-3 py-2 rounded-md">About Me</a>
-                <a href="#Projects" className="hover:bg-blue-700 px-3 py-2 rounded-md">My Projects</a>
-                <a href="#ContactMe" className="hover:bg-blue-700 px-3 py-2 rounded-md">Contact</a>
+      <nav className="bg-violet-500 text-white p-4 fixed w-full flex font-semibold text-2xl">
+        <div className = "container flex justify-start items-center">
+            <div class = "mr-3">
+                <img class = "h-14 rounded-full" src = {require('../Assets/EggMan.jpg')} alt = "Rylan Cox" />
+            </div>
+            <span className="font-mono font-extrabold">Rylan Cox</span>
+        </div>
+        <div className="container flex justify-end">
+            <div className="flex space-x-4 justify-evenly items-center whitespace-nowrap">
+                <a href="#Introduction" className="hover:bg-violet-900 px-3 py-2 rounded-md">Introduction</a>
+                <a href="#AboutMe" className="hover:bg-violet-900 px-3 py-2 rounded-md">About Me</a>
+                <a href="#Projects" className="hover:bg-violet-900 px-3 py-2 rounded-md">My Projects</a>
+                <a href="#ContactMe" className="hover:bg-violet-900 px-3 py-2 rounded-md">Contact</a>
             </div>
         </div>
       </nav>
