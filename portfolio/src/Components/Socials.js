35662eb1c41f11c5b0d45ecd6483817551280d83
Rylan Cox <rylanrcox@gmail.com>
2024-01-28 15:54:23 -0800

    Attempt to correctly position socials in mobile

--- portfolio/src/Components/Socials.js
+++ portfolio/src/Components/Socials.js
@@ -7,7 +7,7 @@ const Socials = () => {
     const linkedinUrl = "https://www.linkedin.com/in/rylancox/";
 
     return (
-        <div className="fixed left-0 md:top-1/2 transform md:-translate-y-1/2 z-50">
+        <div className="fixed top-full left-0 md:top-1/2 transform md:-translate-y-1/2 z-50">
             <div className="bg-violet-500 text-white p-4 flex md:flex-col justify-center md:items-start w-full md:w-4/5">
                 <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="block mb-2 md:mb-2 md:mr-4">
                     <FaGithub size={iconSize} />
